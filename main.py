25f50eacee3df5f4ec2f2dadc827ab1bf2b98223
paraigor <103633539+paraigor@users.noreply.github.com>
2024-09-28 14:52:06 +0300

    Download book covers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from urllib.parse import urljoin, urlsplit, unquote
 
 import requests
 from bs4 import BeautifulSoup
@@ -11,11 +12,45 @@ def check_for_redirect(response):
         raise HTTPError
 
 
-book_folder = Path("books")
-book_folder.mkdir(exist_ok=True)
+def download_txt(url, filename, folder="books"):
+    book_folder = Path(folder)
+    book_folder.mkdir(exist_ok=True)
+
+    book_filename = f"{sanitize_filename(filename)}.txt"
+    book_path = book_folder / book_filename
+
+    try:
+        response = requests.get(url, allow_redirects=True)
+        response.raise_for_status()
+        check_for_redirect(response)
+    except (ConnectionError, HTTPError):
+        return
+
+    with open(book_path, "wb") as file:
+        file.write(response.content)
+
+    return book_path
+
+
+def download_img(url, filename, folder="books"):
+    book_folder = Path(folder)
+    book_folder.mkdir(exist_ok=True)
+
+    img_filename = f"{sanitize_filename(unquote(filename))}"
+    img_path = book_folder / img_filename
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(img_path, "wb") as file:
+        file.write(response.content)
+
+    return img_path
+
 
 book_text_url = "https://tululu.org/txt.php"
 book_page_url = "https://tululu.org/b"
+book_site_url = "https://tululu.org"
 
 for book_id in range(1, 11):
     try:
@@ -28,18 +63,30 @@ for book_id in range(1, 11):
     soup = BeautifulSoup(response.text, "lxml")
     title_text = soup.find("h1").text
     title = title_text.split("::")[0].strip()
-    book_filename = f"{book_id}. {sanitize_filename(title)}.txt"
-    book_path = book_folder / book_filename
+    # book_filename = f"{book_id}. {sanitize_filename(title)}.txt"
+    # book_path = book_folder / book_filename
 
-    payload = {"id": book_id}
-    try:
-        response = requests.get(
-            book_text_url, params=payload, allow_redirects=False
-        )
-        response.raise_for_status()
-        check_for_redirect(response)
-    except (ConnectionError, HTTPError):
+    # payload = {"id": book_id}
+    # try:
+    #     response = requests.get(
+    #         book_text_url, params=payload, allow_redirects=False
+    #     )
+    #     response.raise_for_status()
+    #     check_for_redirect(response)
+    # except (ConnectionError, HTTPError):
+    #     continue
+
+    # with open(book_path, "wb") as file:
+    #     file.write(response.content)
+    book_img = soup.find("div", class_="bookimage")
+    if book_img:
+        img = book_img.find("img")["src"]
+    else:
         continue
 
-    with open(book_path, "wb") as file:
-        file.write(response.content)
+    img_url = urljoin(book_site_url, img)
+    img_filename = urlsplit(img_url)[2].split("/")[-1]
+
+    download_img(img_url, img_filename)
+
+
--- tululu.py
+++ tululu.py
@@ -1,16 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
+from requests.exceptions import ConnectionError, HTTPError
 
-url = "https://tululu.org/b1/"
-response = requests.get(url)
+book_page_url = "https://tululu.org/b"
+url = book_page_url + str(1)
+
+# for book_id in range(1, 11):
+#     try:
+#         url = book_page_url + str(book_id)
+#         response = requests.get(url)
+#         response.raise_for_status()
+#     except (ConnectionError, HTTPError):
+#         continue
+
+#     soup = BeautifulSoup(response.text, "lxml")
+
+#     title_text = soup.find("h1").text
+#     title_splitted = title_text.split("::")
+#     title = title_splitted[0].strip()
+#     img = soup.find("div", class_="bookimage").find("img")["src"]
+
+#     print("Заголовок:", title)
+#     print(img)
+
+response = requests.get(url, allow_redirects=False)
 response.raise_for_status()
 
 soup = BeautifulSoup(response.text, "lxml")
+img = soup.find("div", class_="bookimage").find("img")["src"]
 
-title_text = soup.find("h1").text
-title_splitted = title_text.split("::")
-title = title_splitted[0].strip()
-author = title_splitted[1].strip()
-
-print("Заголовок:", title)
-print("Автор:", author)
+print(img)

ff7326d0d609c2890e3458e38786bc1cb0b2e6ab
paraigor <103633539+paraigor@users.noreply.github.com>
2024-09-28 20:04:26 +0300

    Add parse book page function

--- main.py
+++ main.py
@@ -48,56 +48,51 @@ def download_img(url, filename, folder="books"):
     return img_path
 
 
-book_text_url = "https://tululu.org/txt.php"
-book_page_url = "https://tululu.org/b"
-book_site_url = "https://tululu.org"
-
-for book_id in range(1, 11):
-    try:
-        url = book_page_url + str(book_id)
-        response = requests.get(url)
-        response.raise_for_status()
-    except (ConnectionError, HTTPError):
-        continue
-
+def parse_book_page(response):
+    sheme, netloc, *_ = urlsplit(response.url)
+    book_site_url = sheme + "://" + netloc
     soup = BeautifulSoup(response.text, "lxml")
-    title_text = soup.find("h1").text
-    title = title_text.split("::")[0].strip()
-    # book_filename = f"{book_id}. {sanitize_filename(title)}.txt"
-    # book_path = book_folder / book_filename
 
-    # payload = {"id": book_id}
-    # try:
-    #     response = requests.get(
-    #         book_text_url, params=payload, allow_redirects=False
-    #     )
-    #     response.raise_for_status()
-    #     check_for_redirect(response)
-    # except (ConnectionError, HTTPError):
-    #     continue
+    header = soup.find("h1").text
+    book_title = header.split("::")[0].strip()
+    book_author = header.split("::")[1].strip()
 
-    # with open(book_path, "wb") as file:
-    #     file.write(response.content)
-    book_img = soup.find("div", class_="bookimage")
-    if book_img:
-        img = book_img.find("img")["src"]
-    else:
-        continue
+    book_img = soup.find("div", class_="bookimage").find("img")["src"]
+    book_img_url = urljoin(book_site_url, book_img)
+    book_img_filename = book_img.split("/")[-1]
 
-    img_url = urljoin(book_site_url, img)
-    img_filename = urlsplit(img_url)[2].split("/")[-1]
-
-    # download_img(img_url, img_filename)
-    print(title)
-    # comments = soup.find_all("div", class_="texts")
-
-    # for comment in comments:
-    #     text = comment.find("span").text
-    #     print(text)
     genres_html = soup.find("span", class_="d_book").find_all("a")
     genres = []
-    for genre in genres_html:
-        text = genre.text
-        genres.append(text)
-    print(genres)
-    print()
+    for genre_html in genres_html:
+        genre = genre_html.text
+        genres.append(genre)
+
+    comments_html = soup.find_all("div", class_="texts")
+    comments = []
+    for comment_html in comments_html:
+        comment = comment_html.find("span").text
+        comments.append(comment)
+
+    content = {
+        "book_title": book_title,
+        "book_author": book_author,
+        "book_img_url": book_img_url,
+        "book_img_filename": book_img_filename,
+        "genres": genres,
+        "comments": comments,
+    }
+
+    return content
+
+
+book_text_url = "https://tululu.org/txt.php"
+book_page_url = "https://tululu.org/b9/"
+
+try:
+    response = requests.get(book_page_url)
+    response.raise_for_status()
+    check_for_redirect(response)
+except (ConnectionError, HTTPError):
+    print("Ошибочка")
+
+print(parse_book_page(response))

6fb41ab54f884532856149155273c3496395f1e9
paraigor <103633539+paraigor@users.noreply.github.com>
2024-09-29 22:43:18 +0300

    Update code

--- tululu.py
+++ tululu.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 from pathlib import Path
 from urllib.parse import unquote, urljoin
 
@@ -7,26 +8,32 @@ from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from requests.exceptions import ConnectionError, HTTPError
 
+logging.basicConfig(
+    format="%(levelname)s: %(message)s",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
 
 def check_for_redirect(response):
-    if response.status_code != 200:
+    if response.history:
         raise HTTPError
 
 
-def download_txt(url, filename, folder="books"):
+def download_txt(book_id, filename, folder="books"):
     book_folder = Path(folder)
     book_folder.mkdir(exist_ok=True)
 
-    book_filename = f"{sanitize_filename(filename)}.txt"
+    book_filename = f"{book_id} {sanitize_filename(filename)}.txt"
     book_filename = book_filename.replace(" ", "_")
     book_path = book_folder / book_filename
 
-    try:
-        response = requests.get(url, allow_redirects=False)
-        response.raise_for_status()
-        check_for_redirect(response)
-    except (ConnectionError, HTTPError):
-        return
+    url = "https://tululu.org/txt.php"
+    payload = {"id": {book_id}}
+
+    response = requests.get(url, params=payload, timeout=10)
+    response.raise_for_status()
+    check_for_redirect(response)
 
     with open(book_path, "wb") as file:
         file.write(response.content)
@@ -39,11 +46,8 @@ def download_img(url, filename, folder="book_covers"):
     img_filename = f"{sanitize_filename(unquote(filename))}"
     img_path = img_folder / img_filename
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except (ConnectionError, HTTPError):
-        return
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
 
     with open(img_path, "wb") as file:
         file.write(response.content)
@@ -57,20 +61,16 @@ def parse_book_page(response):
     book_author = header.split("::")[1].strip()
 
     book_img = soup.find("div", class_="bookimage").find("img")["src"]
-    book_img_url = urljoin("https://tululu.org", book_img)
+    book_img_url = urljoin(response.url, book_img)
     book_img_filename = book_img.split("/")[-1]
 
     genres_html = soup.find("span", class_="d_book").find_all("a")
-    genres = []
-    for genre_html in genres_html:
-        genre = genre_html.text
-        genres.append(genre)
+    genres = [genre_html.text for genre_html in genres_html]
 
     comments_html = soup.find_all("div", class_="texts")
-    comments = []
-    for comment_html in comments_html:
-        comment = comment_html.find("span").text
-        comments.append(comment)
+    comments = [
+        comment_html.find("span").text for comment_html in comments_html
+    ]
 
     content = {
         "book_title": book_title,
@@ -108,17 +108,27 @@ def main():
     for book_id in range(args.start_id, args.end_id + 1):
         try:
             url = f"https://tululu.org/b{book_id}/"
-            response = requests.get(url, allow_redirects=False)
+            response = requests.get(url, timeout=10)
             response.raise_for_status()
             check_for_redirect(response)
         except (ConnectionError, HTTPError):
+            logger.warning(f"Книги с id={book_id} нет на сайте.")
             continue
 
         content = parse_book_page(response)
-        download_book_url = f"https://tululu.org/txt.php?id={book_id}"
 
-        download_txt(download_book_url, content["book_title"])
-        download_img(content["book_img_url"], content["book_img_filename"])
+        try:
+            download_txt(book_id, content["book_title"])
+        except (ConnectionError, HTTPError):
+            logger.warning(
+                f"Книга <{content['book_title']}>(id={book_id}). Нет текста для скачивания."
+            )
+        try:
+            download_img(content["book_img_url"], content["book_img_filename"])
+        except (ConnectionError, HTTPError):
+            logger.warning(
+                f"Файл обложки <{content['book_img_filename']}>(id={book_id}) отсутствует."
+            )
 
 
 if __name__ == "__main__":

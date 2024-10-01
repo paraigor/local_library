057159918303070c80a7cbe1d97a8b912902db51
paraigor <103633539+paraigor@users.noreply.github.com>
2024-10-01 20:48:51 +0300

    Move exception processing to main()

--- tululu.py
+++ tululu.py
@@ -28,14 +28,9 @@ def download_txt(book_id, filename, folder="books"):
     url = "https://tululu.org/txt.php"
     payload = {"id": {book_id}}
 
-    try:
-        response = requests.get(url, params=payload, allow_redirects=False)
-        response.raise_for_status()
-        check_for_redirect(response)
-    except (ConnectionError, HTTPError):
-        logging.warning(f"Книга <{filename}>. Нет текста для скачивания.")
-        sleep(5)
-        return
+    response = requests.get(url, params=payload, allow_redirects=False)
+    response.raise_for_status()
+    check_for_redirect(response)
 
     with open(book_path, "wb") as file:
         file.write(response.content)
@@ -48,13 +43,8 @@ def download_img(url, filename, folder="book_covers"):
     img_filename = f"{sanitize_filename(unquote(filename))}"
     img_path = img_folder / img_filename
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except (ConnectionError, HTTPError):
-        logging.warning(f"Файл обложки <{filename}> отсутствует.")
-        sleep(5)
-        return
+    response = requests.get(url)
+    response.raise_for_status()
 
     with open(img_path, "wb") as file:
         file.write(response.content)
@@ -125,8 +115,20 @@ def main():
 
         content = parse_book_page(response)
 
-        download_txt(book_id, content["book_title"])
-        download_img(content["book_img_url"], content["book_img_filename"])
+        try:
+            download_txt(book_id, content["book_title"])
+        except (ConnectionError, HTTPError):
+            logging.warning(
+                f"Книга <{content['book_title']}>(id={book_id}). Нет текста для скачивания."
+            )
+            sleep(5)
+        try:
+            download_img(content["book_img_url"], content["book_img_filename"])
+        except (ConnectionError, HTTPError):
+            logging.warning(
+                f"Файл обложки <{content['book_img_filename']}>(id={book_id}) отсутствует."
+            )
+            sleep(5)
 
 
 if __name__ == "__main__":
